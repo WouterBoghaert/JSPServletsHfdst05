0f8b06e2970e6b10b7ae42e5c944dbc672cc2b8e
WouterBoghaert <wouter.boghaert@gmail.com>
2017-10-31 09:14:21 +0100

    Hoofdstuk 10

--- src/be/vdab/servlets/IndexServlet.java
+++ src/be/vdab/servlets/IndexServlet.java
@@ -8,7 +8,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import be.vdab.entities.Adres;
 import be.vdab.entities.Begroeting;
+import be.vdab.entities.Persoon;
 
 @WebServlet("/index.htm")
 public class IndexServlet extends HttpServlet {
@@ -16,6 +18,8 @@ public class IndexServlet extends HttpServlet {
 	private static final String VIEW = "/WEB-INF/JSP/index.jsp";
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("zaakvoerder", new Persoon("Luigi", "Peperone", 7, true,
+				new Adres("Grote markt", "3", 9700, "Oudenaarde")));
 		request.setAttribute("begroeting", new Begroeting());
 		request.getRequestDispatcher(VIEW).forward(request, response);
 	}
